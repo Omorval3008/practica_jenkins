5ac1faed4c2ad4b7be9ab4117424321d3cd1f867
Omorval3008 <omorval3008@g.educaand.es>
2025-02-22 00:11:59 +0100

    APP FUNCIONA 1.1 - TEST SINCRONIZACIÓN AUTO 2 (el 1 falla build react pero detecta cambios en repo)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 import ListaDeTareas from './componentes/ListaDeTareas';
 
 function App() {
   return (
     <div className='aplicacion-tareas'>
-	<h1>TEST SINCRONIZACIÓN AUTOMÁTICA CON GIT</h1> //Se ha cambaido el titulo para ver 
-	//si se refleja el cambio
+	<h1>TEST 2 SINCRONIZACIÓN AUTOMÁTICA CON GIT</h1>
+	{/*Se habian insertado comentarios de forma incorrecta
+	Se ha cambiado el titulo para ver si los cambios se reflejan*/}
       <div className='tareas-lista-principal'>
         <h1>Mi lista de tareas</h1>
         <ListaDeTareas />
