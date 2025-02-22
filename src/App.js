20915367d899365b4727b955d999d431e73f178c
Omorval3008 <omorval3008@g.educaand.es>
2025-02-22 01:56:18 +0100

    APP FUNCIONA 2.5 TEST WEBHOOK 5

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ListaDeTareas from './componentes/ListaDeTareas';
 function App() {
   return (
     <div className='aplicacion-tareas'>
-	<h1>TEST 3 SINCRONIZACIÓN AUTOMÁTICA CON GIT con WEBHOOKS</h1>
+	<h1>TEST 3 SINCRONIZACIÓN AUTOMÁTICA CON GIT con WEBHOOKS 4</h1>
 	{/*Se habian insertado comentarios de forma incorrecta
 	Se ha cambiado el titulo para ver si los cambios se reflejan*/}
       <div className='tareas-lista-principal'>
